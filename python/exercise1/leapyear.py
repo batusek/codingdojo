6557fcc8875927c711cc45f591ed4a6a6de148be
Robert Batusek <robert.batusek@gmail.com>
2020-04-24 20:05:17 +0200

    Sample solution

--- python/exercise1/leapyear.py
+++ python/exercise1/leapyear.py
@@ -1,2 +1,11 @@
 def isLeapYear(year):
-    return True
+    if year % 400 == 0:
+        return True
+
+    if year % 100 == 0:
+        return False
+
+    if year % 4 == 0:
+        return True
+
+    return False
