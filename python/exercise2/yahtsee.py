9577580d942531a1a0bae9f010d258f845ceb25e
Robert Batusek <robert.batusek@gmail.com>
2020-04-27 13:42:57 +0200

    All number rows covered

--- python/exercise2/yahtsee.py
+++ python/exercise2/yahtsee.py
@@ -3,6 +3,10 @@ from enum import Enum
 class Category(Enum):
     ONES = 1
     TWOS = 2
+    THREES = 3
+    FOURS = 4
+    FIVES = 5
+    SIXES = 6
 
 class Numbers(object):
     def __init__(self,num):
@@ -21,7 +25,11 @@ class Yahtsee(object):
     def score(self,category,values):
         switcher={
             Category.ONES:Numbers(1).score,
-            Category.TWOS:Numbers(2).score
+            Category.TWOS:Numbers(2).score,
+            Category.THREES:Numbers(3).score,
+            Category.FOURS:Numbers(4).score,
+            Category.FIVES:Numbers(5).score,
+            Category.SIXES:Numbers(6).score,
         }
         func = switcher.get(category,lambda:None)
         return func(values)
--- python/exercise2/yahtseetest.py
+++ python/exercise2/yahtseetest.py
@@ -12,6 +12,9 @@ class NumberTest(unittest.TestCase):
     def test_score_to_twos_is_equal_to_sum_of_dice_that_read_two(self):
         self.assertEquals(4,Yahtsee().score(Category.TWOS,[2,3,2,4,5]))
 
+    def test_score_to_sixes_is_equal_to_sum_of_dice_that_read_six(self):
+        self.assertEquals(18,Yahtsee().score(Category.SIXES,[2,3,6,6,6]))
+
 
 if __name__ == '__main__':
     unittest.main()
