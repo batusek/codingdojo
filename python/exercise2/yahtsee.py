a9172511b28e962887cdcae0a094df011d97718b
Robert Batusek <robert.batusek@gmail.com>
2020-04-27 13:23:41 +0200

    The first use case implemented

--- python/exercise2/yahtsee.py
+++ python/exercise2/yahtsee.py
@@ -4,5 +4,17 @@ class Category(Enum):
     ONES = 1
 
 class Yahtsee(object):
+    def ones(self,values):
+        sum = 0
+        for v in values:
+            if v==1:
+                sum += 1
+
+        return sum
+
     def score(self,category,values):
-        return True
+        switcher={
+            Category.ONES:self.ones
+        }
+        func = switcher.get(category,lambda:None)
+        return func(values)
