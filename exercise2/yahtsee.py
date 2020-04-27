4e826ff4c28df1fea9a939f720f41cc5e7ee3983
Robert Batusek <robert.batusek@gmail.com>
2020-04-27 13:31:30 +0200

    Second use case solved

--- exercise2/yahtsee.py
+++ exercise2/yahtsee.py
@@ -2,19 +2,26 @@ from enum import Enum
 
 class Category(Enum):
     ONES = 1
+    TWOS = 2
 
-class Yahtsee(object):
-    def ones(self,values):
+class Numbers(object):
+    def __init__(self,num):
+        self.num = num
+    
+    def score(self,values):
         sum = 0
         for v in values:
-            if v==1:
-                sum += 1
+            if v==self.num:
+                sum += self.num
 
         return sum
 
+
+class Yahtsee(object):
     def score(self,category,values):
         switcher={
-            Category.ONES:self.ones
+            Category.ONES:Numbers(1).score,
+            Category.TWOS:Numbers(2).score
         }
         func = switcher.get(category,lambda:None)
         return func(values)
--- exercise2/yahtseetest.py
+++ exercise2/yahtseetest.py
@@ -6,8 +6,11 @@ class NumberTest(unittest.TestCase):
     def test_if_no_number_exists_score_is_zero(self):
         self.assertEquals(0,Yahtsee().score(Category.ONES,[2,3,2,4,5]))
 
-    def test_score_to_ones_is_euqal_to_sum_of_dice_that_read_one(self):
+    def test_score_to_ones_is_equal_to_sum_of_dice_that_read_one(self):
         self.assertEquals(2,Yahtsee().score(Category.ONES,[1,3,1,4,5]))
+
+    def test_score_to_twos_is_equal_to_sum_of_dice_that_read_two(self):
+        self.assertEquals(4,Yahtsee().score(Category.TWOS,[2,3,2,4,5]))
 
 
 if __name__ == '__main__':
