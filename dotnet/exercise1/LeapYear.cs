258ab503a4c52fa8179e82ea641159036de9a5ed
Robert Batusek <robert.batusek@gmail.com>
2020-04-27 17:32:44 +0200

    Sample solution for the first exercise in .Net

--- dotnet/exercise1/LeapYear.cs
+++ dotnet/exercise1/LeapYear.cs
@@ -6,7 +6,13 @@ namespace LeapYear
     {
         public static bool IsLeapYear(int year)
         {
-            throw new NotImplementedException();
+            if (year%400==0)
+                return true;
+
+            if (year%100==0)
+                return false;
+
+            return year%4==0;
         }
     }
 }
