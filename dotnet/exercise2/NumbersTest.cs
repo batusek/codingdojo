d541ba028f31b7e914aff8ce098dcbf15b31288f
Robert Batusek <robert.batusek@gmail.com>
2020-04-30 12:23:59 +0200

    Test 2 added and fails

--- dotnet/exercise2/NumbersTest.cs
+++ dotnet/exercise2/NumbersTest.cs
@@ -10,6 +10,11 @@ namespace Numbers
             Assert.Equal("one",NumbersInWords.PrintNumber(1));
         }
 
+        [Fact]
+        public void TwoReadsAsTwo()
+        {
+            Assert.Equal("two",NumbersInWords.PrintNumber(2));
+        }
     }
 
 }
