d18a667ca79b5e4a8afe3d072f150f1ddf12ad32
Robert Batusek <robert.batusek@gmail.com>
2020-04-30 12:28:34 +0200

    Test 3 failed

--- dotnet/exercise2/NumbersTest.cs
+++ dotnet/exercise2/NumbersTest.cs
@@ -7,6 +7,7 @@ namespace Numbers
         [Theory]
         [InlineData(1, "one")]
         [InlineData(2, "two")]
+        [InlineData(9, "nine")]
         public void PrintNumberUniversalTest(int input, string expected)
         {
             Assert.Equal(expected,NumbersInWords.PrintNumber(input));
