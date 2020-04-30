50ea8dcd734871192f4cce9d22f6fd410d91f933
Robert Batusek <robert.batusek@gmail.com>
2020-04-30 12:36:06 +0200

    Test 4 failed - 20 added

--- dotnet/exercise2/NumbersTest.cs
+++ dotnet/exercise2/NumbersTest.cs
@@ -8,6 +8,7 @@ namespace Numbers
         [InlineData(1, "one")]
         [InlineData(2, "two")]
         [InlineData(19, "nineteen")]
+        [InlineData(20, "twenty")]
         public void PrintNumberUniversalTest(int input, string expected)
         {
             Assert.Equal(expected,NumbersInWords.PrintNumber(input));
