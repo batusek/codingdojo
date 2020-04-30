253e657e482de2432e784e662ffcfe89f92aa66f
Robert Batusek <robert.batusek@gmail.com>
2020-04-30 12:31:09 +0200

    Test 4 fail - getting over 10

--- dotnet/exercise2/NumbersTest.cs
+++ dotnet/exercise2/NumbersTest.cs
@@ -8,6 +8,7 @@ namespace Numbers
         [InlineData(1, "one")]
         [InlineData(2, "two")]
         [InlineData(9, "nine")]
+        [InlineData(10, "ten")]
         public void PrintNumberUniversalTest(int input, string expected)
         {
             Assert.Equal(expected,NumbersInWords.PrintNumber(input));
