406e9fcba1ac6501b5354e53b025b1c581a7bc89
Robert Batusek <robert.batusek@gmail.com>
2020-06-17 16:30:27 +0200

    Test using mocks added

--- python/exercise4/htmlformattertest.py
+++ python/exercise4/htmlformattertest.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import *
 import os
 from htmlformatter import *
 
@@ -19,6 +20,7 @@ class HtmlFormatterTest(unittest.TestCase):
 </body></html>
 """
 
+    # test using a blackbox test that accesses filesystem
     def test_files_are_identical(self):
         data = [
             { "username": "admin", "date": "2020-02-20"},
@@ -30,6 +32,31 @@ class HtmlFormatterTest(unittest.TestCase):
         f = open("output.html","r")
         actual = f.read()
         self.assertEquals(actual,self.expected)
+
+    # test using mocks
+    @patch('htmlformatter.open')
+    def test_writes_to_file(self, patch_open):
+        data = [
+            { "username": "admin", "date": "2020-02-20"}
+        ]
+
+        mock_file = Mock()
+        patch_open.return_value = mock_file
+        HtmlFormatter().printReport(data)
+        calls = [ 
+            call("<html><body>\n"), 
+            call('<table border="1">\n'), 
+            call("<tr><th>Username</th><th>Last login</th></tr>"),
+            call("<tr>\n"),
+            call('<td>admin</td>'),
+            call('<td>2020-02-20</td>'),
+            call("</tr>\n"),
+            call("</tr></table>\n"),
+            call("</body></html>\n")
+            ]
+        mock_file.write.assert_has_calls(calls)
+        mock_file.close.assert_called()
+
 
 if __name__ == '__main__':
     unittest.main()
