036d62d94e698429ceae40ea51110d8263e06629
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 16:58:25 +0100

    Test 5 failed - 20

--- vuejs/exercise2/numbers.test.ts
+++ vuejs/exercise2/numbers.test.ts
@@ -5,7 +5,8 @@ test.each([
     [1, "one"],
     [2, "two"],
     [9, "nine"],
-    [19, "nineteen"]
+    [19, "nineteen"],
+    [20, "twenty"]
 ])("%i prints as %s", (number, expected) => {
     expect(NumbersInWords.printNumber(number)).toBe(expected);
 })
