58028a76f162d5c6107e43d550a9856721a8e640
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 16:50:53 +0100

    2 tests refactored

--- vuejs/exercise2/numbers.test.ts
+++ vuejs/exercise2/numbers.test.ts
@@ -1,10 +1,9 @@
 import { expect, test } from 'vitest'
 import { NumbersInWords } from "./numbers";
 
-test("1 prints as one", () => {
-    expect(NumbersInWords.printNumber(1)).toBe("one");
-})
-
-test("2 prints as two", () => {
-    expect(NumbersInWords.printNumber(2)).toBe("two");
+test.each([
+    [1, "one"],
+    [2, "two"]
+])("%i prints as %s", (number, expected) => {
+    expect(NumbersInWords.printNumber(number)).toBe(expected);
 })
