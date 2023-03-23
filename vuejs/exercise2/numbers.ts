a805552ec44a4984d29db3674a4f7d870b355129
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 17:00:24 +0100

    Test 5 passed - support for 20

--- vuejs/exercise2/numbers.ts
+++ vuejs/exercise2/numbers.ts
@@ -2,6 +2,10 @@ export class NumbersInWords {
     static printNumber(number: number) {
         const ones = ["zero","one","two","threee","four","five","six","seven","eight","nine",
                     "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-        return ones[number];
+
+        if (number<20)
+            return ones[number];
+
+        return "twenty";
     }
 }
