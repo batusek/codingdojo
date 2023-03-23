ffe59d9d34c38ff66bd6611aada07a923817e16a
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 16:57:30 +0100

    Test 4 pass - support 19

--- vuejs/exercise2/numbers.ts
+++ vuejs/exercise2/numbers.ts
@@ -1,6 +1,7 @@
 export class NumbersInWords {
     static printNumber(number: number) {
-        const ones = ["zero","one","two","threee","four","five","six","seven","eight","nine"];
+        const ones = ["zero","one","two","threee","four","five","six","seven","eight","nine",
+                    "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
         return ones[number];
     }
 }
