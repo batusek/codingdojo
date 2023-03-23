77b49bcad64abc60e829aa551b299f6ced5bc404
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 17:06:28 +0100

    Pass - support for 21

--- vuejs/exercise2/numbers.ts
+++ vuejs/exercise2/numbers.ts
@@ -2,10 +2,16 @@ export class NumbersInWords {
     static printNumber(number: number) {
         const ones = ["zero","one","two","threee","four","five","six","seven","eight","nine",
                     "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+        const tens = ["", "", "twenty"];
 
         if (number<20)
             return ones[number];
 
-        return "twenty";
+        var result = tens[Math.floor(number/10)];
+        var remainder = number - 20;
+        if (remainder>0)
+            result += " " + ones[remainder];
+
+        return result;
     }
 }
