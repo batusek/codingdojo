76a5e8508c026bbe7b12f844be09a842bfb20acb
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 16:45:44 +0100

    Test 2 added and fails

--- vuejs/exercise2/numbers.test.ts
+++ vuejs/exercise2/numbers.test.ts
@@ -5,3 +5,6 @@ test("1 prints as one", () => {
     expect(NumbersInWords.printNumber(1)).toBe("one");
 })
 
+test("2 prints as two", () => {
+    expect(NumbersInWords.printNumber(2)).toBe("two");
+})
