dc71cbc33b709bd900dcf0ee2a41ae19f64c3092
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 16:55:50 +0100

    Test 4 failed - in fact 10-19 need to be hardcoded as well

--- vuejs/exercise2/numbers.test.ts
+++ vuejs/exercise2/numbers.test.ts
@@ -4,7 +4,8 @@ import { NumbersInWords } from "./numbers";
 test.each([
     [1, "one"],
     [2, "two"],
-    [9, "nine"]
+    [9, "nine"],
+    [19, "nineteen"]
 ])("%i prints as %s", (number, expected) => {
     expect(NumbersInWords.printNumber(number)).toBe(expected);
 })
