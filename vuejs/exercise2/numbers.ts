9448faafc7fc44c4d5163daacd415e08f6e3b214
Robert Batůšek <robert.batusek@gmail.com>
2023-03-23 17:11:08 +0100

    Passed - support for 99

--- vuejs/exercise2/numbers.ts
+++ vuejs/exercise2/numbers.ts
@@ -2,13 +2,14 @@ export class NumbersInWords {
     static printNumber(number: number) {
         const ones = ["zero","one","two","threee","four","five","six","seven","eight","nine",
                     "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-        const tens = ["", "", "twenty"];
+        const tens = ["", "", "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
 
         if (number<20)
             return ones[number];
 
-        var result = tens[Math.floor(number/10)];
-        var remainder = number - 20;
+        var ten:number = Math.floor(number/10);
+        var result = tens[ten];
+        var remainder = number - ten*10;
         if (remainder>0)
             result += " " + ones[remainder];
 
