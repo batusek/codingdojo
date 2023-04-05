b7b3781d7954df9c1a097cd4c82d39fb197815c3
Robert Batůšek <robert.batusek@gmail.com>
2023-04-05 12:22:29 +0200

    Pass - A group of zeros contracted to one zero

--- vuejs/exercise3/ipv6validator.test.ts
+++ vuejs/exercise3/ipv6validator.test.ts
@@ -3,10 +3,20 @@ import { IPv6Validator } from "./ipv6validator"
 
 test("happy path eight groups filled with non zero octets", () => {
     let input = "123f:123f:123f:123f:123f:123f:123f:123f"
-    expect(IPv6Validator.contract(input),input);
+    expect(IPv6Validator.contract(input)).toEqual(input);
 });
 
 test("double colon is a valid address that cannot be contracted", () => {
     let input = "::"
-    expect(IPv6Validator.contract(input),input);
+    expect(IPv6Validator.contract(input)).toEqual(input);
+});
+
+test("a group of four zeros should be contracted to one zero", () => {
+    let input = "123f:123f:123f:123f:123f:123f:0000:123f"
+    expect(IPv6Validator.contract(input)).toEqual("123f:123f:123f:123f:123f:123f:0:123f");
+});
+
+test("a group of three zeros should be contracted to one zero", () => {
+    let input = "123f:123f:123f:123f:123f:123f:000:123f"
+    expect(IPv6Validator.contract(input)).toEqual("123f:123f:123f:123f:123f:123f:0:123f");
 });
--- vuejs/exercise3/ipv6validator.ts
+++ vuejs/exercise3/ipv6validator.ts
@@ -2,7 +2,17 @@ export class IPv6Validator {
     static isValid(input: string): boolean {
         throw Error("Not implemented");
     }
+
+    static isZero(component:string): boolean {
+       return /^0+$/.test(component) 
+    }
+
     static contract(input: string): string {
-        return input;
-    }    
+        var components = input.split(":")
+
+        for(let i=0; i<components.length; i++)
+            if (IPv6Validator.isZero(components[i]))
+                components[i] = "0"
+        return components.join(":");
+    }
 }
