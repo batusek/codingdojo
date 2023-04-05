ff3e6f310c60d5959fed7ce89ac2b933623b7d5e
Robert Batůšek <robert.batusek@gmail.com>
2023-04-05 12:36:46 +0200

    Fail - uppercase converted to lowercase

--- vuejs/exercise3/ipv6validator.test.ts
+++ vuejs/exercise3/ipv6validator.test.ts
@@ -25,3 +25,8 @@ test("leading zeros are removed", () => {
     let input = "123f:123f:123f:123f:123f:123f:023f:123f"
     expect(new IPv6Address(input).contract()).toEqual("123f:123f:123f:123f:123f:123f:23f:123f");
 });
+
+test("uppercase converted to lowercase", () => {
+    let input = "123F:123F:123F:123F:123F:123F:123F:123F"
+    expect(new IPv6Address(input).contract()).toEqual("123f:123f:123f:123f:123f:123f:123f:123f");
+});
