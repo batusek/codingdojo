ea54b87ab12059c3b3dc79e048a00953308080e0
Robert Batůšek <robert.batusek@gmail.com>
2023-04-05 12:27:39 +0200

    Refactor - static methods to instance methods

--- vuejs/exercise3/ipv6validator.ts
+++ vuejs/exercise3/ipv6validator.ts
@@ -1,17 +1,23 @@
 export class IPv6Validator {
-    static isValid(input: string): boolean {
+    address: string;
+
+    constructor(input:string) {
+        this.address = input;
+    }
+
+    isValid(): boolean {
         throw Error("Not implemented");
     }
 
-    static isZero(component:string): boolean {
+    private isZero(component:string): boolean {
        return /^0+$/.test(component) 
     }
 
-    static contract(input: string): string {
-        var components = input.split(":")
+    contract(): string {
+        var components = this.address.split(":")
 
         for(let i=0; i<components.length; i++)
-            if (IPv6Validator.isZero(components[i]))
+            if (this.isZero(components[i]))
                 components[i] = "0"
         return components.join(":");
     }
