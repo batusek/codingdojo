821daea111b25ba7f06f46fe94cc454ba436fe1c
Robert Batůšek <robert.batusek@gmail.com>
2023-04-05 13:48:09 +0200

    Pass - too many groups

--- vuejs/exercise3/ipv6validator.ts
+++ vuejs/exercise3/ipv6validator.ts
@@ -76,7 +76,11 @@ export class IPv6Address {
 
         var components = this.address.split(":")
 
+
         try {
+            if (components.length>8)
+                throw new IPv6ParsingError("Too many groups")
+
             for(let i=0; i<components.length; i++)
                 components[i] = this.contractComponent(components[i])   
 
