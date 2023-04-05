3480bf851ac6f123d5a08884c89942b733f5fd98
Robert Batůšek <robert.batusek@gmail.com>
2023-04-05 10:06:00 +0200

    Test 2 passed

--- vuejs/exercise3/ipv6validator.test.ts
+++ vuejs/exercise3/ipv6validator.test.ts
@@ -5,3 +5,8 @@ test("happy path eight groups filled with non zero octets", () => {
     let input = "123f:123f:123f:123f:123f:123f:123f:123f"
     expect(IPv6Validator.contract(input),input);
 });
+
+test("double colon is a valid address that cannot be contracted", () => {
+    let input = "::"
+    expect(IPv6Validator.contract(input),input);
+});
