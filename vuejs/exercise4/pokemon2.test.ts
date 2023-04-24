93e6d6a5f87672a945cc596499baac785455bf38
Robert Batůšek <robert.batusek@gmail.com>
2023-04-24 15:59:29 +0200

    Tests fail, but the first mocked call works

--- vuejs/exercise4/pokemon2.test.ts
+++ vuejs/exercise4/pokemon2.test.ts
@@ -1,22 +1,37 @@
-import { expect, test, vi } from 'vitest';
-import { PokemonPedia } from "./pokemon";
+import createFetchMock from 'vitest-fetch-mock';
+import { vi } from 'vitest';
 
-global.fetch = vi.fn()
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+import { expect, test } from 'vitest';
+import { PokemonPedia2 } from "./pokemon2";
+
 
 function createFetchResponse(data) {
     return { json: () => new Promise((resolve) => resolve(data)) }
   }
 
 test("test with mocks", async () => {
-    // const spy = vi.spyOn(global,'fetch').mockResolvedValue({"name": "pikachu"})
-    fetch.mockResolvedValue(createFetchResponse({"name": "pikachu"}));
+    fetch.mockResponse(JSON.stringify(
+        {
+            "name": "raichu", 
+            "species": { "url": "species_url" },
+            "types": [{ "type": { "url": "type_url"}}]
+        })
+    );
 
-    var pedia = new PokemonPedia(); 
-    var data = await pedia.investigate("pikachu")
+    var pedia = new PokemonPedia2(); 
+    var data = await pedia.investigate("pikachu");
+
+    // var data = fetchMocker.mockResponseOnce(
+    //     () => pedia.investigate("pikachu")
+    //         .then((res) => ({"name": "raichu"}))
+    // );
+    console.log(data);
     
-    expect(data.name).toEqual("pikachu");
+    expect(data.name).toEqual("raichu");
     expect(data.damage_class).toEqual("special");
     expect(data.is_legendary).toBeFalsy();
     expect(data.growth_rate).toEqual("medium");
 });
-
