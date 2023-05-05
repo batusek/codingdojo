7908ff146e04afe7454ff0d200e8e3e5a5ff79c3
Robert Batůšek <robert.batusek@gmail.com>
2023-05-05 13:39:42 +0200

    Exercise3 added

---
+++ vuejs/exercise3/ipv6validator.ts
@@ -0,0 +1,8 @@
+export class IPv6Validator {
+    static isValid(input: string): boolean {
+        throw Error("Not implemented");
+    }
+    static contract(input: string): string {
+        throw Error("Not implemented");
+    }    
+}
