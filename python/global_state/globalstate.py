46785354f69ddb0d8cb0f6c447b2d65fc74cc5ac
Robert Batusek <robert.batusek@gmail.com>
2024-09-12 14:35:35 +0200

    Class method turned into a method called by a singleton

--- python/global_state/globalstate.py
+++ python/global_state/globalstate.py
@@ -1,9 +1,19 @@
 from configparser import ConfigParser
+from typing import Self
 
 class Configuration:
     @classmethod
-    def get_value(cls, key: str) -> str:
+    def instance(cls) -> Self:
+        if not hasattr(cls,'_instance'):
+            cls._instance = Configuration()
+        return cls._instance
+
+    def _get_value(self, key: str) -> str:
         config = ConfigParser()
         config.read("env.conf")
         return config.get('Database', key)        
+    
+    @classmethod
+    def get_value(cls, key: str) -> str:
+        return cls.instance()._get_value(key)
     
