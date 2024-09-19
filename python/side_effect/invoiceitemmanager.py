b88c78d52216d7b3bebfe22da3b9711b47b7eb3d
Robert Batusek <robert.batusek@gmail.com>
2024-09-19 14:36:23 +0200

    Side effect solution introduced

--- python/side_effect/invoiceitemmanager.py
+++ python/side_effect/invoiceitemmanager.py
@@ -7,3 +7,18 @@ class InvoiceItemManager:
         with open("invoice_items.csv", "a") as f:
             f.write(f"{quantity};{price};{tax_rate};{total_with_tax}\n")
 
+#After
+class InvoiceItemManagerA:
+    def save_item_total_amount(self, quantity: float, price: float, tax_rate: float) -> None:
+        total = quantity * price
+        tax = total * tax_rate
+        total_with_tax = total + tax
+        
+        line = f"{quantity};{price};{tax_rate};{total_with_tax}\n"
+        self.save(line)
+
+    def save(self, line):
+        with open("invoice_items.csv", "a") as f:
+            f.write(line)
+
+
--- python/side_effect/test_invoiceitemmanager.py
+++ python/side_effect/test_invoiceitemmanager.py
@@ -2,9 +2,21 @@ import os
 import unittest
 
 from invoiceitemmanager import InvoiceItemManager
+from invoiceitemmanager import InvoiceItemManagerA
+
+class TestInvoiceItemManager(InvoiceItemManagerA):
+    def __init__(self):
+        output = ""
+        
+    def save(self,line):
+        self.output = line
 
 class InvoiceItemManagerTest(unittest.TestCase):
-    pass
+    def test_calculation(self):
+        manager = TestInvoiceItemManager()
+        manager.save_item_total_amount(1,3,0.2)
+        total = manager.output.split(";")[-1]
+        self.assertAlmostEqual(float(total),3.6,2)
         
 if __name__ == '__main__':
     unittest.main()        
