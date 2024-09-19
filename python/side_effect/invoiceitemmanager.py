71e7f43f28e4ceb5e69a3c0f064ff7b4db175a7a
Robert Batusek <robert.batusek@gmail.com>
2024-09-19 14:28:34 +0200

    Side effect exercise introduced

---
+++ python/side_effect/invoiceitemmanager.py
@@ -0,0 +1,9 @@
+class InvoiceItemManager:
+    def save_item_total_amount(self, quantity: float, price: float, tax_rate: float) -> None:
+        total = quantity * price
+        tax = total * tax_rate
+        total_with_tax = total + tax
+        
+        with open("invoice_items.csv", "a") as f:
+            f.write(f"{quantity};{price};{tax_rate};{total_with_tax}\n")
+
---
+++ python/side_effect/test_invoiceitemmanager.py
@@ -0,0 +1,10 @@
+import os
+import unittest
+
+from invoiceitemmanager import InvoiceItemManager
+
+class InvoiceItemManagerTest(unittest.TestCase):
+    pass
+        
+if __name__ == '__main__':
+    unittest.main()        
