e2f803cbc4c11e3f6a3aee94468587e1e00d5011
Robert Batusek <robert.batusek@gmail.com>
2024-09-23 07:33:32 +0200

    Baby steps test updated

--- python/baby_steps/test_numbers.py
+++ python/baby_steps/test_numbers.py
@@ -3,7 +3,4 @@ from numbers import NumbersInWords
 
 class NumbersTest(unittest.TestCase):
     def test_one_reads_as_one(self):
-        
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertTrue(False)
