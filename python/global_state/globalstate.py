ef245cf80b5c890fbad3424ea0ccadb8d976bb30
Robert Batusek <robert.batusek@gmail.com>
2024-09-25 06:02:34 +0200

    Global state exercise adjusted for generation of instructions

--- python/global_state/globalstate.py
+++ python/global_state/globalstate.py
@@ -1,7 +1,18 @@
 from configparser import ConfigParser
 from typing import Self
 
+
+    
 class Configuration:
+    # TODO: Turn to Singleton and possibility to set testing instance
+    @classmethod
+    def get_value(cls, key: str) -> str:
+        config = ConfigParser()
+        config.read("global_state/env.conf")
+        return config.get('Database', key)        
+
+# After start
+class ConfigurationReadyToTest:
     @classmethod
     def instance(cls) -> Self:
         if not hasattr(cls,'_instance'):
@@ -20,4 +31,4 @@ class Configuration:
     @classmethod
     def get_value(cls, key: str) -> str:
         return cls.instance()._get_value(key)
-    
+# After end
--- python/global_state/test_globalstate.py
+++ python/global_state/test_globalstate.py
@@ -1,24 +1,33 @@
 from configparser import ConfigParser
 import unittest
-from global_state.globalstate import Configuration
+from global_state.globalstate import *
+
+class GlobalStateTest(unittest.TestCase):
+    def test_get_db_name(self):
+        self.assertEqual(Configuration.get_value("DB_NAME"),"dev")
+
+    def test_get_db_user(self):
+        self.assertEqual(Configuration.get_value("DB_USER"),"developer")
 
 
-class TestableConfiguration(Configuration):
+
+# After start
+class TestableConfiguration(ConfigurationReadyToTest):
     def __init__(self):
         self.config = ConfigParser()
         
     def _get_value(self, key: str) -> str:
         return self.config.get("Database",key)
 
-class ConfigurationTest(unittest.TestCase):
+class ConfigurationTestWithInstance(unittest.TestCase):
     def test_get_db_name(self):
         configuration = TestableConfiguration()
         configuration.config['Database'] = {
             'DB_NAME': 'internalDB',
             'DB_USER': 'internalUser'
         }
-        Configuration.set_instance(configuration)
-        self.assertEqual(Configuration.get_value("DB_NAME"),"internalDB")
+        ConfigurationReadyToTest.set_instance(configuration)
+        self.assertEqual(ConfigurationReadyToTest.get_value("DB_NAME"),"internalDB")
 
     def test_get_db_user(self):
         configuration = TestableConfiguration()
@@ -26,6 +35,6 @@ class ConfigurationTest(unittest.TestCase):
             'DB_NAME': 'internalDB',
             'DB_USER': 'internalUser'
         }
-        Configuration.set_instance(configuration)
-        self.assertEqual(Configuration.get_value("DB_USER"),"internalUser")
-
+        ConfigurationReadyToTest.set_instance(configuration)
+        self.assertEqual(ConfigurationReadyToTest.get_value("DB_USER"),"internalUser")
+# After end
