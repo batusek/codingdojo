ca64ac37c42bee618b914f0918c4c1bbc40e8e2c
Robert Batusek <robert.batusek@gmail.com>
2024-09-25 06:04:01 +0200

    Intermediate step demonstrated

--- python/global_state/globalstate.py
+++ python/global_state/globalstate.py
@@ -11,7 +11,25 @@ class Configuration:
         config.read("global_state/env.conf")
         return config.get('Database', key)        
 
+
 # After start
+class ConfigurationAsSingleton:
+    @classmethod
+    def instance(cls) -> Self:
+        if not hasattr(cls,'_instance'):
+            cls._instance = Configuration()
+        return cls._instance
+
+    def _get_value(self, key: str) -> str:
+        config = ConfigParser()
+        config.read("env.conf")
+        return config.get('Database', key)        
+    
+    @classmethod
+    def get_value(cls, key: str) -> str:
+        return cls.instance()._get_value(key)
+    
+    
 class ConfigurationReadyToTest:
     @classmethod
     def instance(cls) -> Self:
