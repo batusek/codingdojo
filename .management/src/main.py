68dde7aec0d1f69ac5c6974fa9b00e4294dce426
Robert Batusek <robert.batusek@gmail.com>
2024-09-25 06:56:43 +0200

    Management commands uncommented and unused code deleted

--- .management/src/main.py
+++ .management/src/main.py
@@ -22,45 +22,31 @@ def adaptFile(filename: str):
             if writeLine:
                 f.write(line)
 
-def insertLines(filename: str, start: int, excerpt: list[str]):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-
-    with open(filename, "w") as f:
-        for i, line in enumerate(lines):
-            if i == start:
-                for l in excerpt:
-                    f.write(l)
-
-            f.write(line)
-
 
 def python():
     # no adaptation needed for baby steps yet
-    # adaptFile("../python/blob/worldbank.py")
-    # adaptFile("../python/blob/test_worldbank.py")
-    # adaptFile("../python/external_dependencies/test_htmlformatter.py")
-    # adaptFile("../python/global_state/globalstate.py")
-    # adaptFile("../python/global_state/test_globalstate.py")
+    adaptFile("../python/blob/worldbank.py")
+    adaptFile("../python/blob/test_worldbank.py")
+    adaptFile("../python/external_dependencies/test_htmlformatter.py")
+    adaptFile("../python/global_state/globalstate.py")
+    adaptFile("../python/global_state/test_globalstate.py")
     # no adaptation needed for grow my code yet
-    # adaptFile("../python/irritating_parameter/bankconnector.py")
-    # adaptFile("../python/irritating_parameter/test_bankconnector.py")
-    # adaptFile("../python/legacy_intro/legacyintro.py")
-    # adaptFile("../python/legacy_intro/test_legacyintro.py")
-    # adaptFile("../python/refactoring_alphabet/changemethodsignature.py")
-    # adaptFile("../python/refactoring_alphabet/test_changemethodsignature.py")
-    # adaptFile("../python/refactoring_alphabet/extractmethod.py")
-    # adaptFile("../python/refactoring_alphabet/test_extractmethod.py")
-    # adaptFile("../python/refactoring_alphabet/introduceobject.py")
-    # adaptFile("../python/refactoring_alphabet/test_introduceobject.py")
-    # adaptFile("../python/refactoring_alphabet/replaceconstructor.py")
-    # adaptFile("../python/refactoring_alphabet/test_replaceconstructor.py")
-    # adaptFile("../python/side_effect/invoiceitemmanager.py")
-    # adaptFile("../python/side_effect/test_invoiceitemmanager.py")
+    adaptFile("../python/irritating_parameter/bankconnector.py")
+    adaptFile("../python/irritating_parameter/test_bankconnector.py")
+    adaptFile("../python/legacy_intro/legacyintro.py")
+    adaptFile("../python/legacy_intro/test_legacyintro.py")
+    adaptFile("../python/refactoring_alphabet/changemethodsignature.py")
+    adaptFile("../python/refactoring_alphabet/test_changemethodsignature.py")
+    adaptFile("../python/refactoring_alphabet/extractmethod.py")
+    adaptFile("../python/refactoring_alphabet/test_extractmethod.py")
+    adaptFile("../python/refactoring_alphabet/introduceobject.py")
+    adaptFile("../python/refactoring_alphabet/test_introduceobject.py")
+    adaptFile("../python/refactoring_alphabet/replaceconstructor.py")
+    adaptFile("../python/refactoring_alphabet/test_replaceconstructor.py")
+    adaptFile("../python/side_effect/invoiceitemmanager.py")
+    adaptFile("../python/side_effect/test_invoiceitemmanager.py")
     adaptFile("../python/tooling/leapyear.py")
     adaptFile("../python/tooling/test_leapyear.py")
 
 
 python()
-#javaScript()
-
