a7246b88642a1f3f6cf127baa6802a5adccc1b60
Robert Batusek <robert.batusek@gmail.com>
2024-10-17 15:29:06 +0200

    Refactor - TDD demo - more tests added

--- python/demo/test_commas.py
+++ python/demo/test_commas.py
@@ -2,5 +2,8 @@ import unittest
 from demo.commas import NumberFormatter
 
 class NumberFormatterTest(unittest.TestCase):
-    def test_blah(self):
+    def test_one_prints_without_comma(self):
         self.assertEqual(NumberFormatter().format(1),"1")
+
+    def test_ten_prints_without_comma(self):
+        self.assertEqual(NumberFormatter().format(10),"10")
