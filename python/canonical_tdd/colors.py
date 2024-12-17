d5864e00ddfa54de3aebcdef4f5878e2fc43bdbb
Robert Batusek <robert.batusek@gmail.com>
2024-12-17 13:39:29 +0100

    Canonical TDD added to the python version

---
+++ python/canonical_tdd/colors.py
@@ -0,0 +1,11 @@
+# based on https:#codingdojo.org/kata/NearestColor/
+# Instructions:
+# Implement the ColorEvaluator class with the following methods
+# - nearestColor - returns one of the R, G, B
+# - fartherstColor - returns one of the R, G, B
+# - (optional) deal with the situation when there are multiple nearest or furthers colors
+# - (optional) allow symbolic color names as inputs
+#
+# Constraints:
+#  - practice canonical TDD preparing a list of testcases in advance and maintaining it
+#  - choose a test naming convention and keep it for the whole exercise
