a6a4029046ccad401783d46d02c5da276045193d
Robert Batusek <robert.batusek@gmail.com>
2024-12-17 13:54:48 +0100

    Pass - blue leads to blue

--- python/canonical_tdd/test_colors.py
+++ python/canonical_tdd/test_colors.py
@@ -8,3 +8,6 @@ class ColorEvaluatorTest(TestCase):
         
     def test_nearest_color_of_green_is_green(self):
         self.assertEqual(ColorEvaluator("0f0").nearestColor(),"0f0")        
+        
+    def test_nearest_color_of_blue_is_blue(self):
+        self.assertEqual(ColorEvaluator("00f").nearestColor(),"00f")                
