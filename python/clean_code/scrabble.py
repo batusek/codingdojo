805cf20874c4730c6752740c8f1987267b8e0d56
Robert Batusek <robert.batusek@gmail.com>
2025-01-30 10:27:01 +0100

    Basic scrabble counting + letter and word bonuses

---
+++ python/clean_code/scrabble.py
@@ -0,0 +1,55 @@
+
+class Scrabble:
+    def score(self, word: str) -> int:
+        alphabet: dict[str,int] = {
+            'a': 1,
+            'b': 3,
+            'c': 3,
+            'd': 2,
+            'e': 1,
+            'f': 4,
+            'g': 2,
+            'h': 4,
+            'i': 1,
+            'j': 8,
+            'k': 5,
+            'l': 1,
+            'm': 3,
+            'n': 1,
+            'o': 1,
+            'p': 3,
+            'q': 10,
+            'r': 1,
+            's': 1,
+            't': 1,
+            'u': 1,
+            'v': 4,
+            'w': 4,
+            'x': 8,
+            'y': 4,
+            'z': 10
+        }
+        result = 0
+        last_letter = ""
+        for char in word:
+            if char in alphabet:
+                last_letter = char
+                result += alphabet[char]
+                continue
+
+            if char=="*":
+                result += alphabet[last_letter]
+
+            if char=="(":
+                break
+
+        return self._apply_word_bonuses(word, result)
+
+    def _apply_word_bonuses(self, word: str, raw_count: int) -> int:
+        if word[-3:]=="(d)":
+            return raw_count * 2
+
+        if word[-3:]=="(t)":
+            return raw_count * 3
+
+        return raw_count
