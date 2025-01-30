79f960366534b0c558aaa0011a2a21f58bc92dec
Robert Batusek <robert.batusek@gmail.com>
2025-01-30 10:46:09 +0100

    Refactor - letters_used calculated separately

--- python/clean_code/scrabble.py
+++ python/clean_code/scrabble.py
@@ -1,48 +1,48 @@
 
 class Scrabble:
+    alphabet: dict[str, int] = {
+        'a': 1,
+        'b': 3,
+        'c': 3,
+        'd': 2,
+        'e': 1,
+        'f': 4,
+        'g': 2,
+        'h': 4,
+        'i': 1,
+        'j': 8,
+        'k': 5,
+        'l': 1,
+        'm': 3,
+        'n': 1,
+        'o': 1,
+        'p': 3,
+        'q': 10,
+        'r': 1,
+        's': 1,
+        't': 1,
+        'u': 1,
+        'v': 4,
+        'w': 4,
+        'x': 8,
+        'y': 4,
+        'z': 10
+    }
+
     def score(self, word: str) -> int:
-        alphabet: dict[str,int] = {
-            'a': 1,
-            'b': 3,
-            'c': 3,
-            'd': 2,
-            'e': 1,
-            'f': 4,
-            'g': 2,
-            'h': 4,
-            'i': 1,
-            'j': 8,
-            'k': 5,
-            'l': 1,
-            'm': 3,
-            'n': 1,
-            'o': 1,
-            'p': 3,
-            'q': 10,
-            'r': 1,
-            's': 1,
-            't': 1,
-            'u': 1,
-            'v': 4,
-            'w': 4,
-            'x': 8,
-            'y': 4,
-            'z': 10
-        }
+        letters, *suffix = word.split("(")
         score: int = 0
         last_letter = ""
         last_letter_score = 0
-        letters_used: int = 0
-        for char in word:
-            if char in alphabet:
+        for char in letters:
+            if char in self.alphabet:
                 score += last_letter_score
-                letters_used += 1
                 last_letter = char
-                last_letter_score = alphabet[last_letter]
+                last_letter_score = self.alphabet[last_letter]
                 continue
 
             if char=="*":
-                last_letter_score += alphabet[last_letter]
+                last_letter_score += self.alphabet[last_letter]
 
             if char=="^":
                 score -= last_letter_score
@@ -50,10 +50,14 @@ class Scrabble:
             if char=="(":
                 break
 
-        score += alphabet[last_letter]
+        score += self.alphabet[last_letter]
         temp = self._apply_word_bonuses(word, score)
+
+        letters_used = len([char for char in letters if char in self.alphabet])
         return temp if letters_used < 7 else temp + 50
 
+    def _letter_score(self, word: str) -> int:
+        return 0
     def _apply_word_bonuses(self, word: str, raw_count: int) -> int:
         if word[-3:]=="(d)":
             return raw_count * 2
