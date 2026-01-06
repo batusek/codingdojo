de1021d01ccf6eb5800a6aa4e0f5b34caa1df177
Robert Batusek <robert.batusek@gmail.com>
2026-01-06 16:07:55 +0100

    refactor: prepare for chatbot run #2

--- python/clean_code/scrabble.py
+++ python/clean_code/scrabble.py
@@ -30,47 +30,4 @@ class Scrabble:
     }
 
     def score(self, word: str) -> int:
-        word_multiplier, actual_word = self._parse_word_bonuses(word)
-        base_score = self._calculate_base_score(actual_word)
-        
-        letter_count = len(actual_word.replace('*', '').replace('^', ''))
-        
-        bonus = 0
-        if letter_count == 7:
-            bonus = 50
-            
-        return base_score * word_multiplier + bonus
-
-    def _parse_word_bonuses(self, word: str) -> tuple[int, str]:
-        if word.endswith('(d)'):
-            return 2, word[:-3]
-        if word.endswith('(t)'):
-            return 3, word[:-3]
-        return 1, word
-
-    def _calculate_base_score(self, word: str) -> int:
-        score = 0
-        i = 0
-        last_score = 0
-        while i < len(word):
-            letter = word[i]
-            if letter == '*':
-                i += 1
-                continue
-            if letter == '^':
-                score -= last_score
-                last_score = 0
-                i += 1
-                continue
-
-            letter_score = self.alphabet[letter]
-            
-            i += 1
-            letter_multiplier = 1
-            while i < len(word) and word[i] == '*':
-                letter_multiplier += 1
-                i += 1
-            
-            last_score = letter_score * letter_multiplier
-            score += last_score
-        return score
+        return 0
